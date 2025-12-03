b9cf25ecfa7d699e3d4b39dcaacbf0b90cf4384f
talweller123-glitch <talweller123@gmail.com>
2025-12-03 18:11:53 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,7 +1,1 @@
-def create_codon_dict(file_path):
-    codon_dict = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            codon, amino_acid, single_letter, full_name = line.split()
-            codon_dict[codon] = single_letter
-    return codon_dict
+
